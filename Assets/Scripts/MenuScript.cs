4827d6945147e5f4ac928d22f5dd96def2320d8a
Joseph Walker <jwalker.depaul@gmail.com>
2017-05-13 13:24:09 -0500

    Adding all Assets

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    //Canvas and Button are UI Objects
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;//starts game without quit menu
+    }
+
+    public void exitPress()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    //for when we click on "no"
+    public void noPress()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void startLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+    
+}
