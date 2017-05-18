7f978f2f2220448d2a9894cb0d4dd3a386cb751b
Sam Waters <samrolandwaters@yahoo.com>
2017-05-17 23:35:46 -0500

    Added esc key script to exit game

---
+++ Assets/Scripts/exitMenuScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class exitMenuScript : MonoBehaviour
+{
+
+    private static exitMenuScript instance = null;
+
+    private static exitMenuScript GetInstance() { return instance; }
+
+    //Canvas and Button are UI Objects
+    public Canvas quitMenu;
+
+    // Use this for initialization
+    void Start()
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        quitMenu.enabled = false;//starts game without quit menu
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            quitMenu.enabled = true;
+        }
+    }
+
+    //for when we click on "no"
+    public void noPress()
+    {
+        quitMenu.enabled = false;
+    }
+
+    public void backToTitle()
+    {
+        setScene("MG_UIDemo");
+    }
+
+    public void setScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
