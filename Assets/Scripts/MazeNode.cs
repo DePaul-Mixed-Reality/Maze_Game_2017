b60c104fadf8a79167e7e8b5fc5624baccea823d
Cody Nicholson <codynicholson96@gmail.com>
2017-05-23 20:19:04 -0500

    Organized stuff, added mazeNode class

---
+++ Assets/Scripts/MazeNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeNode : MonoBehaviour
+{
+
+	public Vector3 position;
+	public Vector3 face;
+	public GameObject item = null;
+
+    //Six booleans instead of four to avoid ambiguity of which dimension is west/east/north whatever.
+    //Each node ignores two booleans because it ignores one dimension
+    public bool[] walls = { true, true, true, true, true, true};
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
